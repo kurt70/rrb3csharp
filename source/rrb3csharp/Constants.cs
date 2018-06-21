8784dee8926cf95956e1c662c686f937d188d4eb
Kurt Monge <kurt.monge@gmail.com>
2018-06-21 21:24:25 +0200

    Init method ok but not tested

---
+++ source/rrb3csharp/Constants.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace rrb3csharp
+{
+    public class Constants
+    {
+        public const decimal MOTOR_DELAY = 0.2m;
+
+        public const int RIGHT_PWM_PIN = 14;
+        public const int RIGHT_1_PIN = 10;
+        public const int RIGHT_2_PIN = 25;
+        public const int LEFT_PWM_PIN = 24;
+        public const int LEFT_1_PIN = 17;
+        public const int LEFT_2_PIN = 4;
+        public const int SW1_PIN = 11;
+        public const int SW2_PIN = 9;
+        public const int LED1_PIN = 8;
+        public const int LED2_PIN = 7;
+        public const int OC1_PIN = 22;
+        public const int OC2_PIN = 27;
+        public const int OC2_PIN_R1 = 21;
+        public const int OC2_PIN_R2 = 27;
+        public const int TRIGGER_PIN = 18;
+        public const int ECHO_PIN = 23;
+        public const int left_pwm = 0;
+        public const int right_pwm = 0;
+        public const int pwm_scale = 0;
+
+        public const int old_left_dir = -1;
+        public const int old_right_dir = -1;
+    }
+}
